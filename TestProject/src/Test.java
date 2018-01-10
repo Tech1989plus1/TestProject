c2fccd2f822627ace62311bd743bd780b60a1ca4
DNM <DNM@134.154.37.99>
2018-01-10 10:33:41 -0800

    Just added some comments to test.java

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,4 +1,17 @@
-
+/*
+ * How to add githud project to eclipse
+ * ====================================
+ * 
+ * Step1: Create GitHub account and sign in
+ * Step2: Start a project = create a repository
+ * Step3: Start Eclipse
+ * Step4: Goto perspective -> Git repositories and click on add got repository
+ * Step5: Create a project in eclipse
+ * Step6: Do a right click on project > team > share > add to repository
+ * Step7: Commit and push the project to the repository
+ * Step8: Commit and push every change to the repository
+ * 
+ */
 public class Test {
 
 }
